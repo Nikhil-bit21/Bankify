6eab7150ec0037bc107bc871c8f529cb063042ec
Nikhil-bit21 <swaminikhil21@gmail.com>
2024-05-21 16:55:29 +0530

    added payment route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -73,6 +73,71 @@ router.get('/', jwtAuthMiddleware, async (req, res) => {
 
 });
 
+router.post('/payment', async (req, res) => {
+    try {
+        
+        const userData = req.body;
+        const senderAccountNumber = userData.senderAccountNumber;
+        const senderPin = userData.senderPin;
+        const amount = userData.amount;
+        const receiverAccountNumber = userData.receiverAccountNumber;
+
+        if (!senderPin || isNaN(senderAccountNumber) || isNaN(amount) || amount <= 0 || isNaN(receiverAccountNumber)) {
+            return res.status(400).json({ message: 'Invalid input data' });
+        }
+
+        const senderAccount = await Account.findOne({ accountNumber: senderAccountNumber });
+
+        if (!senderAccount) {
+            return res.status(401).json({ message: 'Invalid Sender account number' });
+        }
+
+        // console.log(`Comparing PIN: provided(${senderPin}), stored(${senderAccount.pin})`);
+
+        if (!await senderAccount.comparePin(senderPin)) {
+            return res.status(401).json({ message: "Invalid Sender's account PIN" });
+        }
+
+        const receiverAccount = await Account.findOne({ accountNumber: receiverAccountNumber });
+
+        if (!receiverAccount) {
+            return res.status(401).json({ message: "Invalid Receiver's account number" });
+        }
+
+        const senderBalance = senderAccount.balance;
+
+        if(senderBalance < amount){
+            return res.status(401).json({ message: "Insufficient Balance" });
+        }
+
+        senderAccount.balance -= amount;
+        receiverAccount.balance += amount;
+
+        senderAccount.transactions.push({
+            modeOfPayment: 'upi',
+            type: 'debit',
+            amount: amount,
+            timestamp: new Date()
+        });
+
+        receiverAccount.transactions.push({
+            modeOfPayment: 'upi',
+            type: 'credit',
+            amount: amount,
+            timestamp: new Date()
+        });
+
+        await senderAccount.save();
+        await receiverAccount.save();
+
+
+        res.status(200).json({ message: `Successfully Sended $${amount} from ${senderAccountNumber} to ${receiverAccountNumber}` });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 
 
 
