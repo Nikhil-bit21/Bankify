acdd61b453ac0947d550aa6cde168bc34ee23c00
Nikhil-bit21 <swaminikhil21@gmail.com>
2024-05-22 10:14:16 +0530

    added base route

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const adminRoute = require('./routes/adminRoute');
 const loginRoute = require('./routes/loginRoute')
 const userRoute = require('./routes/userRoute');
 
+app.get('/', async (req,res)=>{
+    try {
+
+        res.status(200).json({ message: `Find all the endpoint details at https://github.com/Nikhil-bit21/Bankify` });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
 app.use('/bank' , loginRoute);
 app.use('/bank/admin' , adminRoute);
 app.use('/bank/user' , userRoute);
