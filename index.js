77162e9845dcbb8ed26e33a61e7f59dd6669081b
Nikhil-bit21 <swaminikhil21@gmail.com>
2024-05-29 11:07:39 +0530

    updated base route and added notion page to it

--- index.js
+++ index.js
@@ -11,15 +11,41 @@ const adminRoute = require('./routes/adminRoute');
 const loginRoute = require('./routes/loginRoute')
 const userRoute = require('./routes/userRoute');
 
-app.get('/', async (req,res)=>{
-    try {
+// app.get('/', async (req,res)=>{
+//     try {
+//         res.redirect('https://nikhilbit21.notion.site/Rentify-Application-3b693b17a9d249c2b1201b3a1e2e7342');
+//         // res.status(200).json({ message: `Find all the endpoint details at https://github.com/Nikhil-bit21/Rentify-api` });
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// })
 
-        res.status(200).json({ message: `Find all the endpoint details at https://github.com/Nikhil-bit21/Bankify` });
+app.get('/', async (req, res) => {
+    try {
+        const url = 'https://nikhilbit21.notion.site/Bankify-Application-1e214b54a8df4ecfae093b3e0da9058e';
+        
+        // Make a HEAD request to check if the page is available
+        const response = await axios.head(url);
+        
+        if (response.status === 200) {
+            // Page is available, redirect to it
+            res.redirect(url);
+        } else {
+            // Page is not available, send the JSON message
+            res.status(200).json({ message: `Find all the endpoint details at https://github.com/Nikhil-bit21/Rentify-api` });
+        }
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
+        // if (err.response && err.response.status === 404) {
+            // Notion page not found, send the JSON message
+            res.status(200).json({ message: `Find all the endpoint details at https://github.com/Nikhil-bit21/Rentify-api` });
+        // } else {
+            // Some other error occurred
+            console.error(err);
+        //     res.status(500).json({ error: 'Internal Server Error' });
+        // }
     }
-})
+});
 
 app.use('/bank' , loginRoute);
 app.use('/bank/admin' , adminRoute);
